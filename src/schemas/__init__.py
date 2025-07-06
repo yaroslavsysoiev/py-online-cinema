35b432ac78fbace26c24369b10ad2d5a2ddb6528
Yaroslav Sysoiev <mrsomeonepromax@gmail.com>
2025-07-06 21:22:03 +0300

    feat(accounts): add Pydantic schemas for auth and account operations

---
+++ src/schemas/__init__.py
@@ -0,0 +1,13 @@
+from schemas.accounts import (
+    UserRegistrationRequestSchema,
+    UserRegistrationResponseSchema,
+    UserActivationRequestSchema,
+    MessageResponseSchema,
+    PasswordResetRequestSchema,
+    PasswordResetCompleteRequestSchema,
+    UserLoginResponseSchema,
+    UserLoginRequestSchema,
+    TokenRefreshRequestSchema,
+    TokenRefreshResponseSchema,
+    ResendActivationRequestSchema,
+)
