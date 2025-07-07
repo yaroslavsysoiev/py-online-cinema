96eb53d6094c9713e8875d442b2745bc8d215b2f
Yaroslav Sysoiev <mrsomeonepromax@gmail.com>
2025-07-07 18:49:33 +0300

    build: add reset db function to session file

--- src/config/dependencies.py
+++ src/config/dependencies.py
@@ -36,6 +36,37 @@ def get_jwt_auth_manager(settings: BaseAppSettings = Depends(get_settings)) -> J
     )
 
 
+def get_accounts_email_notificator(
+        settings: BaseAppSettings = Depends(get_settings)
+) -> EmailSenderInterface:
+    """
+    Retrieve an instance of the EmailSenderInterface configured with the application settings.
+
+    This function creates an EmailSender using the provided settings, which include details such as the email host,
+    port, credentials, TLS usage, and the directory and filenames for email templates. This allows the application
+    to send various email notifications (e.g., activation, password reset) as required.
+
+    Args:
+        settings (BaseAppSettings, optional): The application settings,
+        provided via dependency injection from `get_settings`.
+
+    Returns:
+        EmailSenderInterface: An instance of EmailSender configured with the appropriate email settings.
+    """
+    return EmailSender(
+        hostname=settings.EMAIL_HOST,
+        port=settings.EMAIL_PORT,
+        email=settings.EMAIL_HOST_USER,
+        password=settings.EMAIL_HOST_PASSWORD,
+        use_tls=settings.EMAIL_USE_TLS,
+        template_dir=settings.PATH_TO_EMAIL_TEMPLATES_DIR,
+        activation_email_template_name=settings.ACTIVATION_EMAIL_TEMPLATE_NAME,
+        activation_complete_email_template_name=settings.ACTIVATION_COMPLETE_EMAIL_TEMPLATE_NAME,
+        password_email_template_name=settings.PASSWORD_RESET_TEMPLATE_NAME,
+        password_complete_email_template_name=settings.PASSWORD_RESET_COMPLETE_TEMPLATE_NAME
+    )
+
+
 async def get_current_user(
         request: Request,
         db: AsyncSession = Depends(get_db),
