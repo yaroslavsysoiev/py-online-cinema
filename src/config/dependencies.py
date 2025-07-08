ca157fb417adb1e023669bf94fcbe8fac5efa91d
Yaroslav Sysoiev <mrsomeonepromax@gmail.com>
2025-07-08 14:07:53 +0300

    fix: circular import

--- src/config/dependencies.py
+++ src/config/dependencies.py
@@ -9,8 +9,8 @@ from security.token_manager import JWTAuthManager
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from database import get_db, UserModel
-from database.models.accounts import UserGroupEnum
+from database import get_db
+from database.models.accounts import UserModel, UserGroupEnum
 
 
 def get_jwt_auth_manager(settings: BaseAppSettings = Depends(get_settings)) -> JWTAuthManagerInterface:
@@ -33,37 +33,6 @@ def get_jwt_auth_manager(settings: BaseAppSettings = Depends(get_settings)) -> J
         secret_key_access=settings.SECRET_KEY_ACCESS,
         secret_key_refresh=settings.SECRET_KEY_REFRESH,
         algorithm=settings.JWT_SIGNING_ALGORITHM
-    )
-
-
-def get_accounts_email_notificator(
-        settings: BaseAppSettings = Depends(get_settings)
-) -> EmailSenderInterface:
-    """
-    Retrieve an instance of the EmailSenderInterface configured with the application settings.
-
-    This function creates an EmailSender using the provided settings, which include details such as the email host,
-    port, credentials, TLS usage, and the directory and filenames for email templates. This allows the application
-    to send various email notifications (e.g., activation, password reset) as required.
-
-    Args:
-        settings (BaseAppSettings, optional): The application settings,
-        provided via dependency injection from `get_settings`.
-
-    Returns:
-        EmailSenderInterface: An instance of EmailSender configured with the appropriate email settings.
-    """
-    return EmailSender(
-        hostname=settings.EMAIL_HOST,
-        port=settings.EMAIL_PORT,
-        email=settings.EMAIL_HOST_USER,
-        password=settings.EMAIL_HOST_PASSWORD,
-        use_tls=settings.EMAIL_USE_TLS,
-        template_dir=settings.PATH_TO_EMAIL_TEMPLATES_DIR,
-        activation_email_template_name=settings.ACTIVATION_EMAIL_TEMPLATE_NAME,
-        activation_complete_email_template_name=settings.ACTIVATION_COMPLETE_EMAIL_TEMPLATE_NAME,
-        password_email_template_name=settings.PASSWORD_RESET_TEMPLATE_NAME,
-        password_complete_email_template_name=settings.PASSWORD_RESET_COMPLETE_TEMPLATE_NAME
     )
 
 
