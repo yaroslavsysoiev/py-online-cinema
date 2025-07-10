20361fea731c00f278a07488ed19039a8689a69c
Yaroslav Sysoiev <mrsomeonepromax@gmail.com>
2025-07-10 15:29:00 +0300

    feat: add and update movie-related database models

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -9,11 +9,32 @@ from database.models.accounts import (
     PasswordResetTokenModel,
     RefreshTokenModel
 )
+from database.models.movies import (
+    MovieModel,
+    LanguageModel,
+    ActorModel,
+    GenreModel,
+    CountryModel,
+    MoviesGenresModel,
+    ActorsMoviesModel,
+    MoviesLanguagesModel
+)
 
 from database.session_sqlite import reset_sqlite_database as reset_database
 from database.validators import accounts as accounts_validators
 
 environment = os.getenv("ENVIRONMENT", "developing")
+
+if environment == "testing":
+    from database.session_sqlite import (
+        get_sqlite_db_contextmanager as get_db_contextmanager,
+        get_sqlite_db as get_db
+    )
+else:
+    from database.session_postgresql import (
+        get_postgresql_db_contextmanager as get_db_contextmanager,
+        get_postgresql_db as get_db
+    )
 
 if environment == "testing":
     from database.session_sqlite import (
